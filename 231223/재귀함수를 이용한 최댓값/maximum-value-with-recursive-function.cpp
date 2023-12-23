4b90170dda1e8052dc625af1fc8c55c6c89e4786
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-23 23:13:15 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 1054ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 231223/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 231223/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -2,16 +2,16 @@
 
 int arr[100];
 int max(int x){
-  if(x==1)
-    return arr[x];
+  if(x==0)
+    return arr[0];
   return max(x-1)>arr[x]?max(x-1):arr[x];
 }
-  
+
 
 int main(){
   int n;
   (void)scanf("%d",&n);
-  for(int i=1;i<=n;i++){
+  for(int i=0;i<n;i++){
     (void)scanf("%d",&arr[i]);
   }
   printf("%d",max(n));
