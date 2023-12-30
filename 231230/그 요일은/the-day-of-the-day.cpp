a4aaa13064f78499efb856bfd8b8b2ba1a12ae7a
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-30 23:51:11 +0900

    Title: 그 요일은, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 231230/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int m1,d1;
+  int m2,d2;
+  string s;
+  (void)scanf("%d %d  %d %d",&m1,&d1,&m2,&d2);
+  cin >> s;
+  string str[8]={"0","Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+  int months[13]={0,31,29,31,30,31,30,31,31,30,31,30,31};
+  int day=1;
+  int month=m1;
+  int ans=0;
+  while(1){
+    d1++;
+    day++;
+    if(str[day]==s)
+      ans++;
+    if(d1>months[month]){
+      d1=1;
+      m1++;
+      month++;
+    }
+    if(day>7)
+      day=1;
+    if(m1==m2&&d1==d2){
+      break;}
+  }
+  cout << ans;
+}
