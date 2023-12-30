cca57d8a0840ac534ed2d5fb2cf66487b9b45f63
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-30 22:41:58 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231230/Date to Date/date-to-date.cpp
+++ 231230/Date to Date/date-to-date.cpp
@@ -12,7 +12,9 @@ int main(){
   for(int i=m1;i<=m2;i++){
     total+=months[i];
   }
-  if(m1==m2&&d1==d2)
+  if(m1==m2&&d1==d2){
     total=1;
-  printf("%d",total-a);
+    printf("%d",total);}
+  else
+    printf("%d",total-a);
 }
