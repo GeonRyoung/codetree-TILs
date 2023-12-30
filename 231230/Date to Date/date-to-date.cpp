3e9f868d2d69d94fcad1f7ec47daf19113dcd4cc
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-30 22:40:22 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/Date to Date/date-to-date.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int m1,d1,m2,d2;
+  (void)scanf("%d %d %d %d",&m1,&d1,&m2,&d2);
+  int months[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+  int a=months[m1]+d1;
+  int total=d2;
+  for(int i=m1;i<=m2;i++){
+    total+=months[i];
+  }
+  printf("%d",total-a);
+}
