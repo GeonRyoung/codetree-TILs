1bd78e3fab9e3d3036d756d0b3716d6874a93128
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-30 23:06:12 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/DateTime to DateTime/datetime-to-datetime.cpp
+++ 231230/DateTime to DateTime/datetime-to-datetime.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
+bool safe(int d1,int h1,int m1,int d2,int h2,int m2){
+  if(d1>d2||(d1==d2&&h1>h2)||(d1==d2&&h1==h2&&m1>m2))
+    return true;
+  return false;
+}
+
 int main(){
   int d1=11,h1=11,m1=11;
   int d2,h2,m2;
   int total_min=0;
   (void)scanf("%d %d %d",&d2,&h2,&m2);
   while(true){
+    if(safe(d1,h1,m1,d2,h2,m2)){
+      printf("-1");
+      break;}
     if(d1==d2 && h1==h2 && m1==m2)
       break;
     total_min++;
@@ -22,5 +31,7 @@ int main(){
       h1=0;
     }
   }
-  printf("%d",total_min);
+  if(safe(d1,h1,m1,d2,h2,m2)==0){
+    printf("%d",total_min);
+  }
 }
