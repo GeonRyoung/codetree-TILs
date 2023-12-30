5cec172f389310ef146eec8c804922424f5e17d8
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-30 22:48:53 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/Date to Date/date-to-date.cpp
+++ 231230/Date to Date/date-to-date.cpp
@@ -7,14 +7,14 @@ int main(){
   int m1,d1,m2,d2;
   (void)scanf("%d %d %d %d",&m1,&d1,&m2,&d2);
   int months[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-  int a=months[m1]+d1;
-  int total=d2;
-  for(int i=m1;i<=m2;i++){
-    total+=months[i];
+  int f_total=d1;
+  int a_total=d2;
+  for(int i=1;i<m1;i++){
+    f_total+=months[i];
   }
-  if(m1==m2&&d1==d2){
-    total=1;
-    printf("%d",total);}
-  else
-    printf("%d",total-a);
+  for(int i=1;i<m2;i++){
+    a_total+=months[i];
+  }
+  printf("%d",a_total-f_total+1);
+  return 0;
 }
