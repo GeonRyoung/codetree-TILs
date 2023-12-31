c2b894652da535ee6a4582ed735ce66d22b68590
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-31 23:13:37 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231231/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int n;
+  (void)scanf("%d",&n);
+  int arr[300]={};
+  for(int i=0;i<n;i++){
+    int a,b;
+    (void)scanf("%d %d",&a,&b);
+      for(int i=a;i<=b;i++)
+        arr[i]+=1;
+  }  
+  int max=0;
+  for(int i=1;i<200;i++){
+    if(arr[i]>max)
+      max=arr[i];
+  }
+  printf("%d",max);
+  return 0;
+}
