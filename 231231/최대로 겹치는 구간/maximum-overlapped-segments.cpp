0bbae992fc9a76d12f10d20c21ba1ce7ed44c290
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2023-12-31 23:12:36 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231231/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 231231/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -3,28 +3,13 @@
 int main(){
   int n;
   (void)scanf("%d",&n);
-  int arr[200]={};
+  int arr[300]={};
   for(int i=0;i<n;i++){
     int a,b;
     (void)scanf("%d %d",&a,&b);
-    if(a>0&&b>0){
-      for(int i=a;i<b;i++){
+      for(int i=a+100;i<b+100;i++)
         arr[i]+=1;
-      }
-    }
-    else if(a<0&&b<0){
-      for(int i=101;i<=(-1*a)+100;i++){
-        arr[i]+=1;
-      }
-      for(int i=1;i<b;i++){
-        arr[i]+=1;
-      }
-    }
-    else{
-      for(int i=(-1*b)+100;i<(-1*a)+100;i++)
-        arr[i]+=1;
-    }
-  }
+  }  
   int max=0;
   for(int i=1;i<200;i++){
     if(arr[i]>max)
