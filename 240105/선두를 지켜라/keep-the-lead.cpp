b3e7a250d34d1fc6e27948bedfbdd6ceaf3ffca0
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-05 23:46:17 +0900

    Title: 선두를 지켜라, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240105/선두를 지켜라/keep-the-lead.cpp
+++ 240105/선두를 지켜라/keep-the-lead.cpp
@@ -1,4 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <algorithm>
+
+using namespace std;
 
 double pos_n[1000001];
 double pos_m[1000001];
@@ -9,24 +12,24 @@ int main(){
   int ans=0;
   (void)scanf("%d %d",&N,&M);
   for(int i=0;i<N;i++){
-    int v,t;
+    double v,t;
     double one_hour;
-    (void)scanf("%d %d",&v,&t);
+    (void)scanf("%lf %lf",&v,&t);
     one_hour=t/v;
     for(int j=0;j<v;j++){
       pos_n[time_n++]+=one_hour;
     }
   }
   for(int i=0;i<M;i++){
-    int v,t;
+    double v,t;
     double one_hour;
-    (void)scanf("%d %d",&v,&t);
+    (void)scanf("%lf %lf",&v,&t);
     one_hour=t/v;
     for(int j=0;j<v;j++){
       pos_m[time_m++]+=one_hour;
     }
   }
-  for(int i=2;i<time_n;i++){
+  for(int i=2;i<max(time_n,time_m);i++){
     if(pos_n[i-1]>pos_m[i-1]&&pos_n[i]<pos_m[i])
       ans+=1;
     else if(pos_n[i-1]<pos_m[i-1]&&pos_n[i]>pos_m[i])
