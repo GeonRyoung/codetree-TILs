0fde34012f2acd2803a5f3b1e0ab7f894e49d85f
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-07 13:58:35 +0900

    Title: 선두를 지켜라, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240107/선두를 지켜라/keep-the-lead.cpp
+++ 240107/선두를 지켜라/keep-the-lead.cpp
@@ -8,12 +8,11 @@ int main(){
   int time_n=1;
   int time_m=1;
   int ans=0;
-  int leader=0;
   (void)scanf("%d %d",&n,&m);
   for(int i=0;i<n;i++){
     int v,t;
     (void)scanf("%d %d",&v,&t);
-    while(t--){
+    for(int i=0;i<t;i++){
       pos_n[time_n]=pos_n[time_n-1]+v;
       time_n++;
     }
@@ -21,22 +20,25 @@ int main(){
   for(int i=0;i<m;i++){
     int v,t;
     (void)scanf("%d %d",&v,&t);
-    while(t--){
+    for(int i=0;i<t;i++){
       pos_m[time_m]=pos_m[time_m-1]+v;
       time_m++;
     }
   }
-  for(int i = 1; i < time_n; i++) {
-      if(pos_n[i] > pos_m[i]) {
-          if(leader == 2)
-              ans++;
-          leader = 1; 
-      }
-      else if(pos_n[i] < pos_m[i]) {
-          if(leader == 1)
-              ans++;
-          leader = 2; 
-      }
+  for(int i=1;i<time_n;i++){
+    if(pos_n[i]>pos_m[i])
+      compare[i]=1;
+    else if(pos_n[i]<pos_m[i])
+      compare[i]=-1;
+    else
+      compare[i]=0;
+  }
+  for(int i=2;i<time_n;i++){
+    if(compare[i-1]*compare[i]==-1)
+      ans++;
+    if(i>=3&&compare[i-2]*compare[i]==-1&&compare[i-1]==0)
+      ans++;
   }
   printf("%d",ans);
+  
 }
