61164400d37701a96a2ae0b49b34afd2117e168c
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-07 22:50:45 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240107/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int infect[101];
+int infect_cnt[101];
+
+class Infect{
+  public:
+    int t,x,y,cnt;
+    Infect(int t,int x,int y){
+      this->t=t;
+      this->x=x;
+      this->y=y;
+    }
+  Infect(){}
+};
+
+bool cmp(Infect a,Infect b){
+  if(a.t<b.t)
+    return true;
+  else
+    return false;
+}
+
+int main(){
+  int N,K,P,T;
+  (void)scanf("%d %d %d %d",&N,&K,&P,&T);
+  infect[P-1]=1;
+  Infect infect_list[100];
+  for(int i=0;i<T;i++){
+    int t,x,y;
+    (void)scanf("%d %d %d",&t,&x,&y);
+    infect_list[i]=Infect(t,x-1,y-1);
+    }
+  sort(infect_list,infect_list+T,cmp);  
+  for(int i=0;i<T;i++){
+    if(infect_cnt[infect_list[i].x]<K&&infect[infect_list[i].x]==1){
+      infect_cnt[infect_list[i].x]++;
+      infect[infect_list[i].y]=1;
+    }
+    if(infect_cnt[infect_list[i].y]<K&&infect[infect_list[i].y]==1){
+      infect_cnt[infect_list[i].y]++;
+      infect[infect_list[i].x]=1;
+    }
+  }
+  for(int i=0;i<N;i++){
+    printf("%d",infect[i]);
+  }
+  return 0;
+}
