d8b66e8ce411c9ecb3c5db3af5224757e4a8f518
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-09 22:57:34 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/되돌아오기/come-back.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int n;
+int arr[101][101];
+int visit[101][101];
+int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
+
+int dire(char x){
+  if(x=='E') return 0;
+  else if(x=='S') return 1;
+  else if(x=='W') return 2;
+  else return 3;
+}
+
+
+int main(){
+  (void)scanf("%d",&n);
+  int x=1,y=1,cnt=0;;
+  for(int i=0;i<n;i++){
+    char dir;int dis;
+    (void)scanf(" %c %d",&dir,&dis);
+    int dir_num=dire(dir);
+    for(int i=0;i<dis;i++){
+      x=x+dx[dir_num],y=y+dy[dir_num];
+      cnt++;
+      if(x==1&&y==1){
+        printf("%d",cnt);
+        break;
+      }
+    }
+  }
+  if(x!=1||y!=1) printf("-1");
+  return 0;
+}
