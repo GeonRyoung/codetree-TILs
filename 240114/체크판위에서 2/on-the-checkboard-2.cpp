a8822379bdfa5619382a64c008bff79ebf247718
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-14 22:09:27 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240114/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240114/체크판위에서 2/on-the-checkboard-2.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-char arr[16][16];
+char arr[15][15];
 
 int main(){
   int r,c;
   (void)scanf("%d %d",&r,&c);
-  for(int i=1;i<=r;i++){
-    for(int j=1;j<=c;j++){
-      (void)scanf("%c",&arr[i][j]);
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++){
+      cin >> arr[i][j];
     }
   }
   int cnt=0;
-  for(int i=2;i<=r;i++){
-    for(int j=2;j<c;j++){
-      for(int k=i+1;k<=r-1;k++){
-        for(int l=j+1;l<=c-1;l++){
-          if(arr[1][1]!=arr[i][j]&&arr[i][j]!=arr[k][l]&&arr[k][l]!=arr[r][c])
+  for(int i=1;i<r;i++){
+    for(int j=1;j<c;j++){
+      for(int k=i+1;k<r-1;k++){
+        for(int l=j+1;l<c-1;l++){
+          if(arr[0][0]!=arr[i][j]&&arr[i][j]!=arr[k][l]&&arr[k][l]!=arr[r-1][c-1])
             cnt++;
         }
       }
