3637842df2b6caac638e3e7edbf85afd3117db01
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-29 23:01:48 +0900

    Title: 특정 구간의 원소 평균값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[101];
+
+int main(){
+  int n;
+  int cnt=0;
+  (void)scanf("%d",&n);
+  for(int i=1;i<=n;i++)
+    (void)scanf("%d",&arr[i]);
+  for(int i=1;i<=n;i++){
+    for(int j=i;j<=n;j++){
+      int sum=0;
+      for(int k=i;k<=j;k++){
+        sum+=arr[k];
+      }
+      double mid_val=(double)sum/(j-i+1);
+      for(int k=1;k<=n;k++){
+        if(arr[k]==mid_val){
+          cnt++;
+          break;
+        }
+      }
+    }
+  }
+  printf("%d",cnt);
+  return 0;
+}
