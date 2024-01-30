2e768210f4e531b648b665bd5d96691c461ef921
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-30 22:34:50 +0900

    Title: 아름다운 수열 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240130/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240130/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -14,20 +14,24 @@ int main(){
   for(int i=1;i<=m;i++){
     int a;
     (void)scanf("%d",&a);
-    if(point[a]==1) continue;
-    else point[a]++;
+    point[a]++;
   }
   int cnt=0;
   for(int i=1;i<=n-m+1;i++){
-    int sum=0;
     for(int j=1;j<=n;j++){
       visit[j]=point[j];
     }
     for(int j=i;j<i+m;j++){
-      sum+=visit[arr[j]];
-      visit[arr[j]]=0;
+      visit[arr[j]]--;
     }
-    if(sum==m)
+    bool check=true;
+    for(int j=1;j<=MAX_NUM;j++){
+      if(visit[j]!=0){
+        check=false;
+        break;
+      }
+    }
+    if(check)
       cnt++;
   }
   printf("%d",cnt);
