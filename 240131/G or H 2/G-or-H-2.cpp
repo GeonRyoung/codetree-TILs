0ee0742bab7e0af5f0800ecddebff3e1fed89ce5
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-31 22:45:54 +0900

    Title: G or H 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <algorithm>
+#include <climits>
+
+#define MAX_NUM 101
+
+using namespace std;
+
+int arr[101];
+
+int main(){
+  int n;
+  (void)scanf("%d",&n);
+  for(int i=1;i<=n;i++){
+    int a;char b;
+    (void)scanf(" %d %c",&a,&b);
+    if(b=='G') arr[a]=1;
+    else if(b=='H') arr[a]=2;
+  }
+  int ans=INT_MIN;
+  for(int i=1;i<=MAX_NUM;i++){
+    for(int j=i+1;j<=MAX_NUM;j++){
+      if(arr[i]==0 || arr[j]==0) continue;
+      int cnt_g=0,cnt_h=0;
+      for(int k=i;k<=j;k++){
+        if(arr[k]==1) cnt_g++;
+        if(arr[k]==2) cnt_h++;
+      }
+      if(cnt_g==0||cnt_h==0||cnt_g==cnt_h){
+        ans=max(ans,(j-i));
+      }
+    }
+  }
+  printf("%d",ans);
+  return 0;
+}
