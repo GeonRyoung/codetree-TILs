55a9c31da10e01d24aea92d1a1fb3aff2ababfd6
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-01-31 23:00:05 +0900

    Title: 개발자의 능력 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/개발자의 능력 3/ability-of-developer-3.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[7];
+
+int average(int i,int j,int k){
+  int sum1=arr[i]+arr[j]+arr[k];
+  int sum2=0;
+  for(int i=1;i<=6;i++){
+    sum2+=arr[i];
+  }
+  sum2-=sum1;
+  return abs(sum1-sum2);
+}
+
+int main(){
+  for(int i=1;i<=6;i++){
+    (void)scanf("%d",&arr[i]);
+  }
+  int cnt=1000000;
+  for(int i=1;i<=6;i++){
+    for(int j=i+1;j<=6;j++){
+      for(int k=j+1;j<=6;j++){
+        cnt=min(cnt,average(i,j,k));
+      }
+    }
+  }
+  printf("%d",cnt);
+  return 0;
+}
