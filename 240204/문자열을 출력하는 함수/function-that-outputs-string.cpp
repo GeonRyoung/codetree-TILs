461bc734c3b063265d81222f16ec03eafbb91fec
GeonRyoung <136659941+GeonRyoung@users.noreply.github.com>
2024-02-04 21:59:10 +0900

    Title: 문자열을 출력하는 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240204/문자열을 출력하는 함수/function-that-outputs-string.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+  string str="CodeTree!";
+  int n;
+  cin >> n;
+  for(int i=1;i<=n;i++){
+    cout << str << endl;
+  }
+  return 0;
+}
